cb3c7b1e2c7a16e1ed30f7ff47b9dd1b58d28821
PramodMandati <pramod.mandati@gmail.com>
2021-01-24 19:49:09 +0530

    'removed'

--- HRManagement/settings.py
+++ HRManagement/settings.py
@@ -15,8 +15,8 @@ import os
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mail.tesing24@gmail.com'
-EMAIL_HOST_PASSWORD = 'gfymdmiwmzlrqbkq'
+EMAIL_HOST_USER = os.environ.get('email')
+EMAIL_HOST_PASSWORD = os.environ.get('email_password')
 EMAIL_USE_TLS = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -29,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gw2&cez=!+ftf9$lkxjyy%inxlm1%(ray)mh9dze@s6!ym^ucl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['hr--management.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['hr--management.herokuapp.com']
 
 # Application definition
 
